42ab9ee508ad1b8eada29162f3a7941b28147e53
Marcos khabie <marcos.khabie@ing.austral.edu.ar>
2017-03-13 23:37:17 -0300

    excercise3

--- src/main/tp1/Exercise3.java
+++ src/main/tp1/Exercise3.java
@@ -6,42 +6,58 @@ package main.tp1;
 public class Exercise3 {
 
 
-    public  static int[] merge (int[] arrayA,int[]arrayB){
-        int[] arrayC= new int[arrayA.length+arrayB.length];
+    public static int[] merge(int[] arrayA, int[] arrayB) {
+        int[] arrayC = new int[arrayA.length + arrayB.length];
 
-        int indiceA;
-        int indiceB;
-        int indiceC;
-        for (indiceA=0;indiceA<arrayA.length;){
-            for (indiceB=0;indiceB<arrayB.length;){
-                for (indiceC=0;indiceC<arrayC.length;indiceC++){
-                    if (arrayA[indiceA]<arrayB[indiceB]){
-                        arrayC[indiceC]=arrayA[indiceA];
-                        indiceA++;
-                    }
-                    else arrayC[indiceC]=arrayB[indiceB];
+        int indiceA = 0;
+        int indiceB = 0;
+        int indiceC = 0;
+    
+        while (indiceA < arrayA.length && indiceB < arrayB.length) {
+            if (arrayA[indiceA] < arrayB[indiceB]) {
+                arrayC[indiceC] = arrayA[indiceA];
+                indiceA++;
+            } else {
+                arrayC[indiceC] = arrayB[indiceB];
+                indiceB++;
+            }
+            indiceC++;
+
+        }
+
+        if (indiceA == arrayA.length) {
+            for (; indiceB < arrayB.length; ) {
+                for (; indiceC < arrayC.length; indiceC++) {
+
+                    arrayC[indiceC] = arrayB[indiceB];
                     indiceB++;
                 }
 
             }
-        }
-        if (arrayA.length<arrayB.length){
-            for (indiceB=0;indiceB<arrayB.length;indiceB++){
-                for (indiceC=arrayA.length;indiceC<arrayC.length;indiceC++){
-                    if (arrayC[indiceC]<arrayB[indiceB]){
 
-                    }
+        } else {
+            for (; indiceA < arrayA.length; ) {
+                for (; indiceC < arrayC.length; indiceC++) {
 
-                else{
-                        arrayC[indiceC]=arrayB[indiceB];
-                    }
-
+                    arrayC[indiceC] = arrayA[indiceA];
+                    indiceA++;
                 }
 
             }
-
+            return arrayC;
         }
-return arrayC;
+        return arrayC;
+    }
+
+    public static void main(String[] args) {
+
+
+        int[] a = {1,4,7};
+        int[] b = {5,6,8,10};
+
+      Exercise2.printArray( merge(a,b));
+
     }
 
 }
+
