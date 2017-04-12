0f6a5a6bad9bf00b74c8fd5588c91a67478f9453
Agustin Bettati <bettatiagustin@gmail.com>
2017-04-12 12:23:06 -0300

    javadoc finished

--- src/main/queues/simulation/Strategy.java
+++ src/main/queues/simulation/Strategy.java
@@ -9,5 +9,10 @@ package main.queues.simulation;
  * The capacity of arranging the flow of new clients that enter a bank.
  */
 public interface Strategy {
+    /**
+     * Enqueues a given amount of clients to a bank.
+     * @param aBank
+     * @param amountOfClients
+     */
     void enqueue(Bank aBank,int amountOfClients);
 }
