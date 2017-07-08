f1f2fefd97a174ff1e3f650a27c51d5161769e6f
Agustin Bettati <bettatiagustin@gmail.com>
2017-07-08 18:44:06 -0300

    Created classes

---
+++ src/main/binaryFiles/Main.java
@@ -0,0 +1,11 @@
+package main.binaryFiles;
+
+/**
+ * @author Agustin Bettati
+ * @version 1.0
+ */
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/binaryFiles/Student.java
@@ -0,0 +1,8 @@
+package main.binaryFiles;
+
+/**
+ * @author Agustin Bettati
+ * @version 1.0
+ */
+public class Student {
+}
